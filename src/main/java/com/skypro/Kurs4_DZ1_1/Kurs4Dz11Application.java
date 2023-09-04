0427a1c21998e901d6e2b94d6e99bff4d29447e9
action0709 <action0709@mail.ru>
2023-09-04 19:40:23 +0400

    1

---
+++ src/main/java/com/skypro/Kurs4_DZ1_1/Kurs4Dz11Application.java
@@ -0,0 +1,13 @@
+package com.skypro.Kurs4_DZ1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kurs4Dz11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kurs4Dz11Application.class, args);
+	}
+
+}
